aafa7b6b2fd548d39f12e3248c8811c8998cb556
alejoboga20 <alejoboga19@gmail.com>
2024-01-02 16:12:35 -0300

    add a chain

--- README.md
+++ README.md
@@ -7,3 +7,17 @@ This is a simple CLI tool to generate code snippets from a given text using GPT.
 ```bash
 poetry install
 ```
+
+## Usage
+
+1. Initiate the virtual environment
+
+```bash
+poetry shell
+```
+
+2. Run the CLI
+
+```bash
+python main.py
+```
--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 from langchain.llms import OpenAI
+from langchain.prompts import PromptTemplate
+from langchain.chains import LLMChain
 
 api_key = ""
 
@@ -6,6 +8,20 @@ llm = OpenAI(
     openai_api_key=api_key
 )
 
+code_prompt = PromptTemplate(
+    template="Write a very short {language} function that will {task}",
+    input_variables=['language', 'task']
+)
+
+code_chain = LLMChain(
+    llm=llm,
+    prompt=code_prompt,
+)
+
 # Generate text
-result = llm('write a poem')
-print(result)
+result = code_chain({
+    'language': 'python',
+    'task': 'print Hello World'
+})
+
+print(result['text'])
